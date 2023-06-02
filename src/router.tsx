a19c5de748f65f7e8fc92043645989e62d254f23
ChaDongmin <cdm1263@gmail.com>
2023-06-02 18:44:21 +0900

    사용자 페이지 내의 하위 페이지 중첩 라우팅

--- src/router.tsx
+++ src/router.tsx
@@ -2,9 +2,16 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import AdminPage from "./pages/admin";
+
+// auth
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
+
+// user
 import UserPage from "./pages/user";
+import LikePage from "./pages/user/LikePage";
+import AccountPage from "./pages/user/AccountPage";
+import PersonalSettings from "./pages/user/PersonalSettings";
 
 function Router() {
   const [username, setUsername] = useState("");
@@ -17,9 +24,19 @@ function Router() {
           element={<App username={username} setUsername={setUsername} />}
         />
         <Route path="/admin" element={<AdminPage />} />
-        <Route path="/auth/login" element={<LoginPage setUsername={setUsername} />} />
-        <Route path="/auth/register" element={<RegisterPage setUsername={setUsername} />} />
-        <Route path="/user" element={<UserPage />} />
+        <Route
+          path="/auth/login"
+          element={<LoginPage setUsername={setUsername} />}
+        />
+        <Route
+          path="/auth/register"
+          element={<RegisterPage setUsername={setUsername} />}
+        />
+        <Route path="/user" element={<UserPage />}>
+          <Route path="account" element={<AccountPage />} />
+          <Route path="like" element={<LikePage />} />
+          <Route path="settings" element={<PersonalSettings />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
