3635082673e0ea7cd06f06f94a393c41136d26fe
pildrums <there7788@gmail.com>
2023-06-02 17:03:48 +0900

    커스텀 버튼 초기버전 완료 & 스타일링 철민님과 협의 후 작업시작

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,5 +1,33 @@
-function Button() {
-  return <div>Button</div>;
+import styled, { css } from "styled-components";
+
+interface IButtonProps {
+  [props: string]: any;
 }
+
+function Button({ ...props }: IButtonProps) {
+  return <StyledButton {...props} />;
+}
+
+const StyledButton = styled.button<{ auth?: boolean; changeAuth?: boolean }>`
+  border: none;
+  outline: none;
+  cursor: pointer;
+  background: #707070;
+  color: #fff;
+  ${(props) =>
+    props.auth &&
+    css`
+      padding: 0.5rem 1rem;
+      width: 100%;
+      font-size: 1.125rem;
+    `}
+  ${(props) =>
+    props.changeAuth &&
+    css`
+      padding: 0.5rem 1rem;
+      background: #333;
+      color: #fff;
+    `}
+`;
 
 export default Button;
