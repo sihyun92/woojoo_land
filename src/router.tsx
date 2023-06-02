283af976979a3f5b1c81f2cec5edc298dc1980c7
pildrums <there7788@gmail.com>
2023-06-02 15:03:38 +0900

    username, setUsername 초기 상태 정의
    > App 컴포넌트에 username, setUsername props 전달
    > auth 컴포넌트에 setUsername 전달

--- src/router.tsx
+++ src/router.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import AdminPage from "./pages/admin";
@@ -5,13 +6,17 @@ import AuthPage from "./pages/auth";
 import UserPage from "./pages/user";
 
 function Router() {
+  const [username, setUsername] = useState("");
   return (
     <BrowserRouter>
       <Routes>
         {/* path name은 임시로 지었습니다 */}
-        <Route path="/" element={<App />} />
+        <Route
+          path="/"
+          element={<App username={username} setUsername={setUsername} />}
+        />
         <Route path="/admin" element={<AdminPage />} />
-        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/auth" element={<AuthPage setUsername={setUsername} />} />
         <Route path="/user" element={<UserPage />} />
       </Routes>
     </BrowserRouter>
