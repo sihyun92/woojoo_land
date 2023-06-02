4080c66792a25cc4c3feacee1191d9ed3359deb2
pildrums <there7788@gmail.com>
2023-06-02 15:06:03 +0900

    interface 정의 및 MainPage 컴포넌트로 props 전달
    > MainPage에 username, setUsername 데이터 전달
    > 전달을 위한 인터페이스 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Dispatch, SetStateAction } from "react";
 import MainPage from "./pages/main";
 
-function App() {
-  return <MainPage />;
+interface IAppProps {
+  username: string;
+  setUsername: Dispatch<SetStateAction<string>>;
+}
+
+function App({ username, setUsername }: IAppProps) {
+  return <MainPage username={username} setUsername={setUsername} />;
 }
 
 export default App;
