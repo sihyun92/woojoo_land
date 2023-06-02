589d9cfdb63ad9dab8bea643b35fdecb2ecb03e5
pildrums <there7788@gmail.com>
2023-06-02 15:16:06 +0900

    auth 기능 구현 완료

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,13 +1,19 @@
-import { FormEvent, useState } from "react";
+import { Dispatch, FormEvent, SetStateAction, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { login, logout } from "../../lib/API/authAPI";
+import { login } from "../../lib/API/authAPI";
+
+// interface
+interface IAuthPageProps {
+  setUsername: Dispatch<SetStateAction<string>>;
+}
 
 // component
-function AuthPage() {
+function AuthPage({setUsername}: IAuthPageProps) {
   // hooks
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
 
   // function
   const onChange = (event: FormEvent) => {
@@ -26,19 +32,13 @@ function AuthPage() {
     setUsername(username || "");
     setEmail("");
     setPassword("");
-  };
-  const onLogout = async () => {
-    await logout();
-    localStorage.removeItem("Token");
-    localStorage.removeItem("username");
-    setUsername("");
+    navigate("/");
   };
 
   // render
   return (
-    <>
-      <h1>{username}</h1>
-      <form onSubmit={onSubmit}>
+    <AuthContainer>
+      <AuthForm onSubmit={onSubmit}>
         <input
           type="text"
           required
@@ -55,13 +55,25 @@ function AuthPage() {
           onChange={onChange}
           name="password"
         />
-        <button type="submit">확인</button>
-        <button onClick={onLogout}>로그아웃</button>
-      </form>
-    </>
+        <button type="submit">로그인</button>
+      </AuthForm>
+    </AuthContainer>
   );
 }
 
-const AuthContainer = styled.div``;
+const AuthContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2.5rem;
+`;
+
+const AuthForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  input {
+    padding: 0.5rem 1rem;
+  }
+`;
 
 export default AuthPage;
