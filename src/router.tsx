5b9a3a8886c0f0acdb4c8e2fa793779f9608bfa0
Kled <1017yu@daum.net>
2023-06-02 17:53:38 +0900

    브라우저라우터 수정

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
 import AdminPage from "./pages/admin";
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/Register";
 import UserPage from "./pages/user";
 
-function Router() {
+function TheRouter() {
   const [username, setUsername] = useState("");
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         {/* path name은 임시로 지었습니다 */}
         <Route
@@ -17,12 +17,18 @@ function Router() {
           element={<App username={username} setUsername={setUsername} />}
         />
         <Route path="/admin" element={<AdminPage />} />
-        <Route path="/auth/login" element={<LoginPage setUsername={setUsername} />} />
-        <Route path="/auth/register" element={<RegisterPage setUsername={setUsername} />} />
+        <Route
+          path="/auth/login"
+          element={<LoginPage setUsername={setUsername} />}
+        />
+        <Route
+          path="/auth/register"
+          element={<RegisterPage setUsername={setUsername} />}
+        />
         <Route path="/user" element={<UserPage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
-export default Router;
+export default TheRouter;
