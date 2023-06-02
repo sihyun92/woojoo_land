e3935964a3203c97ade45cdf1ad43564ba97e80b
pildrums <there7788@gmail.com>
2023-06-02 15:07:25 +0900

    로그인 상태에 따른 로그인 로그아웃 조건부 렌더링
    > username이 있을 때는 username과 로그아웃 버튼 렌더링
    > 그게 아닌 경우 로그인 버튼 렌더링

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,5 +1,32 @@
-function MainPage() {
-  return <div>MainPage</div>;
+import { Dispatch, SetStateAction } from "react";
+import { Link } from "react-router-dom";
+import { logout } from "../../lib/API/authAPI";
+
+interface IMainPageProps {
+  username: string;
+  setUsername: Dispatch<SetStateAction<string>>;
+}
+
+function MainPage({ username, setUsername }: IMainPageProps) {
+  const onLogout = async () => {
+    await logout();
+    localStorage.removeItem("Token");
+    localStorage.removeItem("username");
+    setUsername("");
+  };
+  return (
+    <div>
+      <h1>MainPage</h1>
+      {username ? (
+        <>
+          <h2>{username}</h2>
+          <button onClick={onLogout}>로그아웃</button>
+        </>
+      ) : (
+        <Link to="/auth">로그인</Link>
+      )}
+    </div>
+  );
 }
 
 export default MainPage;
