4f2c8b5391b68b930c8ea660f354407ffc45a008
Kled <1017yu@daum.net>
2023-06-05 14:30:40 +0900

    임시 theme style 작성

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -2,6 +2,14 @@ import "styled-components";
 
 declare module "styled-components" {
   export interface DefaultTheme {
-    colors: {};
+    colors: {
+      purple: string;
+      white: string;
+      lime: string;
+      black: string;
+      gray: string;
+      pink: string;
+      blue: string;
+    };
   }
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,13 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  colors: {},
+  colors: {
+    purple: "#922bff",
+    black: "#000",
+    white: "#fff",
+    gray: "#858585",
+    lime: "#bae313",
+    pink: "#f4e9ff",
+    blue: "#545BFC",
+  },
 };
