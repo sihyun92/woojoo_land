0bf31a0bf24167e4576590ffa8df5537c0f8c221
Kled <1017yu@daum.net>
2023-06-05 14:29:18 +0900

    ThemeProvider 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import GlobalStyle from "./styles/GlobalStyle";
 import Router from "./router";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <Router />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router />
+    </ThemeProvider>
   </React.StrictMode>,
 );
