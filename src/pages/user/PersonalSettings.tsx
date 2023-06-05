7dcbbea6eac52e9bd94d8958caf090d62d6e1acc
ChaDongMin <cdm1263@gmail.com>
2023-06-05 19:11:19 +0900

    사용자 정보 수정 기능 일부 구현

--- src/pages/user/PersonalSettings.tsx
+++ src/pages/user/PersonalSettings.tsx
@@ -1,5 +1,73 @@
+import { ChangeEvent, useState } from "react";
+import styled from "styled-components";
+import { userUpdate } from "../../lib/API/authAPI";
+
 function PersonalSettings() {
-  return <div>개인 정보 수정 페이지</div>;
+  const [user, setUser] = useState({});
+
+  const onSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    userUpdate(user);
+  };
+
+  const onChangeForm = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUser((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  return (
+    <SettingsRoute>
+      <h2>개인 정보 수정</h2>
+      <form>
+        <div>
+          <span>이름</span>
+          <input
+            onChange={onChangeForm}
+            name="displayName"
+            type="text"
+            placeholder=""
+          />
+        </div>
+        <div>
+          <span>기존 비밀번호</span>
+          <input
+            onChange={onChangeForm}
+            name="oldPassword"
+            type="password"
+            placeholder=""
+          />
+        </div>
+        <div>
+          <span>새 비밀번호</span>
+          <input
+            onChange={onChangeForm}
+            name="newPassword"
+            type="password"
+            placeholder=""
+          />
+        </div>
+        {/* <div>
+          <span>프로필 이미지</span>
+        </div> */}
+        {Object.keys(user).length ? (
+          <button type="submit" onClick={onSubmit}>
+            저장
+          </button>
+        ) : (
+          ""
+        )}
+      </form>
+    </SettingsRoute>
+  );
 }
 
+const SettingsRoute = styled.main`
+  display: flex;
+  flex-direction: column;
+`;
 export default PersonalSettings;
