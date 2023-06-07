27a57af2fa306492f0aa58d15f7a24e10154e2c4
pildrums <there7788@gmail.com>
2023-06-07 15:30:28 +0900

    fullWidth 속성 추가 -> 변경 예정

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -8,7 +8,7 @@ function Button({ ...props }: IButtonProps) {
   return <StyledButton {...props} />;
 }
 
-const StyledButton = styled.button<{ active?: boolean }>`
+const StyledButton = styled.button<{ active?: boolean; fullWidth?: boolean }>`
   border: none;
   outline: none;
   cursor: pointer;
@@ -21,7 +21,14 @@ const StyledButton = styled.button<{ active?: boolean }>`
       padding: 0.5rem 1rem;
       background: #333;
       color: #fff;
-    `} 
+    `}
+  ${(props) =>
+    props.fullWidth &&
+    css`
+      padding: 0.75rem 0;
+      width: 100%;
+      font-size: 1.125rem;
+    `}
 `;
 
 export default Button;
