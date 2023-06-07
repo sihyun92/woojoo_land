e3dd343b4ffad2d8219d21ef08868b201cd69947
Kled <1017yu@daum.net>
2023-06-07 22:01:35 +0900

    Header 링크 수정, 디자인 작업

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,16 +1,36 @@
+// import { Link } from "react-router-dom";
 import { theme } from "../../styles/theme";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { IoMdHeartEmpty } from "react-icons/io";
+import { MdOutlineShoppingCart, MdSearch } from "react-icons/md";
 
 function Header() {
   return (
     <HeaderContainer>
       <HeaderWrapper>
-        <AuthWrapper>로그인 | 회원가입</AuthWrapper>
-        <MainWrapper>
-          <Search>Search</Search>
-          <ButtonWrapper>하트 장바구니 프로필</ButtonWrapper>
-        </MainWrapper>
+        <LogoWrapper>우주 부동산</LogoWrapper>
+        <SearchWrapper>
+          <SearchInput type="text" />
+          <MdSearch />
+        </SearchWrapper>
+        <UserWrapper>
+          <AuthWrapper>
+            <a href="/auth/login" target="_blank" rel="noopener noreferrer">
+              로그인
+            </a>
+            <span>|</span>
+            회원가입
+          </AuthWrapper>
+          <ButtonWrapper>
+            <button>
+              <IoMdHeartEmpty />
+            </button>
+            <button>
+              <MdOutlineShoppingCart />
+            </button>
+            <button>프로필</button>
+          </ButtonWrapper>
+        </UserWrapper>
       </HeaderWrapper>
     </HeaderContainer>
   );
@@ -21,22 +41,59 @@ const HeaderContainer = styled.header`
   background: ${theme.colors.purple};
   display: flex;
   justify-content: center;
+  align-items: center;
 `;
 
 const HeaderWrapper = styled.div`
-  max-width: 1200px;
-  width: 1200px;
-  height: inherit;
+  max-width: 75rem;
+  width: 75rem;
+  height: 5rem;
   background: gray;
   display: flex;
-  flex-direction: column;
   position: relative;
+  justify-content: space-between;
+  align-items: center;
 `;
-const AuthWrapper = styled.div``;
-const MainWrapper = styled.div`
+
+const LogoWrapper = styled.div``;
+const SearchWrapper = styled.div`
   display: flex;
+  position: relative;
+  svg {
+    font-size: 1.5rem;
+    position: absolute;
+    right: 0;
+    top: calc((3rem - 1.5rem) / 2);
+  }
 `;
-const Search = styled.div``;
-const ButtonWrapper = styled.div``;
+
+const SearchInput = styled.input`
+  width: 26.25rem;
+  height: 3rem;
+  border: 2px solid #bbb;
+  border-radius: 0.625rem;
+`;
+
+const UserWrapper = styled.div`
+  display: flex;
+  width: 8.25rem;
+  flex-direction: column;
+  height: 100%;
+`;
+const AuthWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  margin-top: auto;
+  justify-content: space-between;
+
+  > button {
+    width: 2.5rem;
+    height: 2.5rem;
+  }
+`;
 
 export default Header;
