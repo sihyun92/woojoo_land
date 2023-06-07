3e482615880f64c69784d3e89054c755307371f6
Kled <1017yu@daum.net>
2023-06-07 19:39:11 +0900

    Common 헤더 태그 삭제

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,40 +1,29 @@
 import { theme } from "../../styles/theme";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import Tag from "./Tag";
 
 function Header() {
   return (
-    <TheHeader>
-      <HeaderContainer>
+    <HeaderContainer>
+      <HeaderWrapper>
         <AuthWrapper>로그인 | 회원가입</AuthWrapper>
         <MainWrapper>
-          <Link to="/">우주부동산</Link>
           <Search>Search</Search>
           <ButtonWrapper>하트 장바구니 프로필</ButtonWrapper>
         </MainWrapper>
-        <TagWrapper>
-          <Tag inherit>홈</Tag>
-          <Tag>#태양계</Tag>
-          <Tag>#안드로메다</Tag>
-          <Tag>#우주복</Tag>
-          <Tag>#우주식량</Tag>
-          <Tag>#우주선</Tag>
-          <Tag>#블랙홀</Tag>
-        </TagWrapper>
-      </HeaderContainer>
-    </TheHeader>
+      </HeaderWrapper>
+    </HeaderContainer>
   );
 }
-const TheHeader = styled.header`
+const HeaderContainer = styled.header`
   width: 100%;
-  height: 12.5rem;
+  height: 9.375rem;
   background: ${theme.colors.purple};
   display: flex;
   justify-content: center;
 `;
 
-const HeaderContainer = styled.div`
+const HeaderWrapper = styled.div`
   max-width: 1200px;
   width: 1200px;
   height: inherit;
@@ -49,8 +38,5 @@ const MainWrapper = styled.div`
 `;
 const Search = styled.div``;
 const ButtonWrapper = styled.div``;
-const TagWrapper = styled.div`
-  display: flex;
-`;
 
 export default Header;
