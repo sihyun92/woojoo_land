3be27ef1b773d8650ffd181bd3ae9561b11faf6a
ChaDongMin <cdm1263@gmail.com>
2023-06-08 17:06:53 +0900

    임시 기본 스타일링

--- src/pages/user/AccountPage.tsx
+++ src/pages/user/AccountPage.tsx
@@ -1,18 +1,14 @@
 import { useEffect, useState } from "react";
 import { accountDisconnect, myAccount } from "../../lib/API/userAPI";
 import styled from "styled-components";
+import UserTitle from "../../components/user/UserTitle";
 
-interface ResponseValue {
-  totalBalance: number; // 사용자 계좌 잔액 총합
-  accounts: Bank[]; // 사용자 계좌 정보 목록
-}
-
-interface Bank {
-  id: string; // 계좌 ID
-  bankName: string; // 은행 이름
-  bankCode: string; // 은행 코드
-  accountNumber: string; // 계좌 번호
-  balance: number; // 계좌 잔액
+interface IBank {
+  id: string;
+  bankName: string;
+  bankCode: string;
+  accountNumber: string;
+  balance: number;
 }
 
 function AccountPage() {
@@ -48,34 +44,37 @@ function AccountPage() {
 
   return (
     <AccountRoute>
-      <h2>계좌 관리</h2>
+      <UserTitle>계좌 관리</UserTitle>
       <AccountListBox>
         {accounts
-          ? accounts.map((account: Bank) => {
+          ? accounts.map((account: IBank) => {
               return (
                 <AccountList key={account.id}>
-                  <BankName>{account.bankName}</BankName>
-                  <AccountNumber>{account.accountNumber}</AccountNumber>
-                  <Balance>{account.balance}</Balance>
-                  <button
+                  <AccountWrapper>
+                    <BankName>{account.bankName}</BankName>
+                    <AccountNumber>{account.accountNumber}</AccountNumber>
+                    <Balance>{account.balance}</Balance>
+                  </AccountWrapper>
+                  <DelAccount
                     onClick={(event) => {
                       delAccount(event, account.id);
                     }}
                   >
                     x
-                  </button>
+                  </DelAccount>
                 </AccountList>
               );
             })
           : "연결된 계좌가 없습니다"}
       </AccountListBox>
-      <button onClick={newAccount}>계좌 추가</button>
+      <AddAccount onClick={newAccount}>계좌 추가</AddAccount>
     </AccountRoute>
   );
 }
 
-const AccountRoute = styled.main`
+const AccountRoute = styled.div`
   display: flex;
+  width: 100%;
   flex-direction: column;
 `;
 
@@ -87,11 +86,29 @@ const AccountListBox = styled.ul`
 const AccountList = styled.li`
   display: flex;
   align-items: center;
-  justify-content: space-between;
   border: 1px solid;
+  padding: 10px 15px;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 5px;
+`;
+
+const AccountWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
 `;
 const BankName = styled.span``;
-const AccountNumber = styled.span``;
+const AccountNumber = styled.span`
+  color: #ff6113;
+`;
 const Balance = styled.span``;
+
+const DelAccount = styled.button`
+  margin-left: 40px;
+`;
+const AddAccount = styled.button`
+  width: 10rem;
+`;
 
 export default AccountPage;
