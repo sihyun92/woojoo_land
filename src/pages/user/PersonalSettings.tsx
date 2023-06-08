642f0ba133e84ec33b69addb56e96e676f4298a8
ChaDongMin <cdm1263@gmail.com>
2023-06-08 17:06:40 +0900

    이메일 표시기능 추가 및 임시 기본 스타일링

--- src/pages/user/PersonalSettings.tsx
+++ src/pages/user/PersonalSettings.tsx
@@ -1,10 +1,21 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import styled from "styled-components";
-import { userUpdate } from "../../lib/API/userAPI";
+import { userUpdate, check } from "../../lib/API/userAPI";
+import UserTitle from "../../components/user/UserTitle";
 
 function PersonalSettings() {
+  const [email, setEmail] = useState("");
   const [user, setUser] = useState({});
   const [error, setError] = useState("");
+
+  useEffect(() => {
+    getEmail();
+  }, []);
+
+  const getEmail = async () => {
+    const res = await check();
+    setEmail(res.email);
+  };
 
   const onSubmit = async (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
@@ -26,35 +37,48 @@ function PersonalSettings() {
 
   return (
     <SettingsRoute>
-      <h2>개인 정보 수정</h2>
-      <form>
-        <div>
-          <span>이름</span>
+      <UserTitle>개인 정보 수정</UserTitle>
+      <SettingsForm>
+        <ImgTitle>
+          <span>프로필 이미지</span>
+        </ImgTitle>
+        <ImgInput></ImgInput>
+        <EmailTitle>
+          <span>이메일</span>
+        </EmailTitle>
+        <EmailViewer>
+          <span>{email}</span>
+        </EmailViewer>
+        <NameTitle>
+          <span>닉네임</span>
+        </NameTitle>
+        <NameInput>
           <input
             onChange={onChangeForm}
             name="displayName"
             type="text"
             placeholder=""
           />
-        </div>
-        <div>
+        </NameInput>
+        <PassWordTitle>
           <span>기존 비밀번호</span>
+          <span>새 비밀번호</span>
+        </PassWordTitle>
+        <PassWordInput>
           <input
             onChange={onChangeForm}
             name="oldPassword"
             type="password"
             placeholder=""
           />
-        </div>
-        <div>
-          <span>새 비밀번호</span>
           <input
             onChange={onChangeForm}
             name="newPassword"
             type="password"
             placeholder=""
           />
-        </div>
+        </PassWordInput>
+
         {/* <div>
           <span>프로필 이미지</span>
         </div> */}
@@ -66,13 +90,60 @@ function PersonalSettings() {
         ) : (
           ""
         )}
-      </form>
+      </SettingsForm>
     </SettingsRoute>
   );
 }
 
-const SettingsRoute = styled.main`
-  display: flex;
+const SettingsRoute = styled.div`
+  width: 100%;
   flex-direction: column;
 `;
+
+const SettingsForm = styled.form`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: 200px 50px 50px 100px;
+
+  // nth child 형태로 만드는게 좋을지
+  // 각각 styled-compontents 형태로 만드는게 좋을지
+  div {
+    display: flex;
+    padding: 10px 20px;
+    border-bottom: 1px solid;
+
+    :nth-child(1),
+    :nth-child(2) {
+      border-top: 1px solid;
+    }
+
+    :nth-child(2n-1) {
+      background-color: #f4f4f4;
+    }
+  }
+`;
+
+const ImgTitle = styled.div``;
+const ImgInput = styled.div``;
+const EmailTitle = styled.div`
+  align-items: center;
+`;
+const EmailViewer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const NameTitle = styled.div`
+  align-items: center;
+`;
+const NameInput = styled.div``;
+const PassWordTitle = styled.div`
+  flex-direction: column;
+  justify-content: space-around;
+`;
+const PassWordInput = styled.div`
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
 export default PersonalSettings;
