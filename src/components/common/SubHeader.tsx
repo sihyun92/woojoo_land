75a023bacdadedfaeede29f6b429b5b9206cc734
Kled <1017yu@daum.net>
2023-06-21 21:57:22 +0900

    Banner 서브헤더에 추가

--- src/components/common/SubHeader.tsx
+++ src/components/common/SubHeader.tsx
@@ -1,19 +1,23 @@
 import styled from "styled-components";
 import Tag from "./Tag";
+import Banner from "../main/MainBanner";
 
 function SubHeader() {
   return (
-    <TagContainer>
-      <TagWrapper>
-        <Tag inherit>홈</Tag>
-        <Tag>#태양계</Tag>
-        <Tag>#안드로메다</Tag>
-        <Tag>#우주복</Tag>
-        <Tag>#우주식량</Tag>
-        <Tag>#우주선</Tag>
-        <Tag>#사건의 지평선</Tag>
-      </TagWrapper>
-    </TagContainer>
+    <>
+      <TagContainer>
+        <TagWrapper>
+          <Tag inherit>ALL</Tag>
+          <Tag>#태양계</Tag>
+          <Tag>#안드로메다</Tag>
+          <Tag>#우주복</Tag>
+          <Tag>#우주식량</Tag>
+          <Tag>#우주선</Tag>
+          <Tag>#사건의 지평선</Tag>
+        </TagWrapper>
+      </TagContainer>
+      <Banner />
+    </>
   );
 }
 
