609767bf1bc381925b7d29b23fd94d5c12108b2a
pildrums <there7788@gmail.com>
2023-06-21 18:59:19 +0900

    리덕스 모듈 생성

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import loading from "./loading";
+import auth from "./auth";
 
 const rootReducer = combineReducers({
   loading,
+  auth,
 });
 
 export default rootReducer;
--- src/modules/loading.ts
+++ src/modules/loading.ts
@@ -1,46 +1,50 @@
+/*
+  2023.06.20
+  - 기존의 리덕스 액션을 typesafe-actions로 변경
+  - 리듀서는 createReducer와 handleReducer로 작성
+*/
+
+import { ActionType, createAction, createReducer } from "typesafe-actions";
+
 // 액션
 const START_LOADING = "loading/START_LOADING" as const;
 const FINISH_LOADING = "loading/FINISH_LOADING" as const;
 
 // 액션 생성 함수
-export const startLoading = () => ({
-  type: START_LOADING,
-});
+export const startLoading = createAction(
+  START_LOADING,
+  (requestType) => requestType,
+)();
 
-export const finishLoading = () => ({
-  type: FINISH_LOADING,
-});
+export const finishLoading = createAction(
+  FINISH_LOADING,
+  (requestType) => requestType,
+)();
 
 // 액션 객체에 대한 타입
-type TLoadingAction =
-  | ReturnType<typeof startLoading>
-  | ReturnType<typeof finishLoading>;
+// 액션 객체 타입은 액션 생성함수를 참조해서 유추가 가능하기 때문에 제너릭을 생략해도 무방.
+const actions = { startLoading, finishLoading }; // 모든 액션 생성함수들을 actions 객체에 넣음
+type TLoadingAction = ActionType<typeof actions>; // ActionType을 사용하여 모든 액션 객체 타입 준비
 
 // 관리할 상태의 타입
 type TLoadingState = {
-  startLoading: boolean;
-  finishLoading: boolean;
+  loading: boolean;
 };
 
 // 초기 상태
 const initialState: TLoadingState = {
-  startLoading: false,
-  finishLoading: true,
+  loading: false,
 };
 
 // 리듀서
-function loading(
-  state: TLoadingState = initialState,
-  action: TLoadingAction,
-): TLoadingState {
-  switch (action.type) {
-    case START_LOADING:
-      return { startLoading: true, finishLoading: false };
-    case FINISH_LOADING:
-      return { startLoading: false, finishLoading: true };
-    default:
-      return state;
-  }
-}
+const loading = createReducer<TLoadingState, TLoadingAction>(initialState)
+  .handleAction(startLoading, (state, action) => ({
+    ...state,
+    [action.payload]: true,
+  }))
+  .handleAction(finishLoading, (state, action) => ({
+    ...state,
+    [action.payload]: false,
+  }));
 
 export default loading;
