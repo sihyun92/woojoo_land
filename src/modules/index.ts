ed73271c578c2eb0747dc4c56ecf981189257322
Kled <1017yu@daum.net>
2023-06-21 16:56:53 +0900

    combineReducer에 cartItem 추가

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,15 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import loading from "./loading";
-import auth, { userSaga } from "./auth";
 import cartItem from "./cartItem";
-import user, { userCheckSaga } from "./user";
-import { all } from "redux-saga/effects";
 
 const rootReducer = combineReducers({
   loading,
-  auth,
   cartItem,
-  user,
 });
 
 export function* rootSaga() {
