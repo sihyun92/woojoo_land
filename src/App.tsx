8c6b461744f94261435542d105dc607b3f967fec
Kled <1017yu@daum.net>
2023-06-22 21:48:56 +0900

    Tag 조건부 렌더링 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 // common
 import Header from "./components/common/Header";
 // import Footer from "./components/common/Footer";
+import Loading from "./components/common/Loading";
 
 // admin
 import AdminPage from "./pages/admin";
@@ -27,34 +28,72 @@ import SettingPage from "./pages/user/SettingPage";
 import MainPage from "./pages/main/MainPage";
 import ProductPage from "./pages/main/ProductPage";
 import CartPage from "./pages/main/CartPage";
-import Loading from "./components/common/Loading";
-import AdminOrderPage from "./pages/admin/AdminOrderPage";
-import AdminOrderPage from "./pages/admin/AdminOrderPage";
+import PaymentPage from "./pages/main/PaymentPage";
+import AllPage from "./pages/main/tagged/AllPage";
+import Solar from "./pages/main/tagged/Solar";
+import Andromeda from "./pages/main/tagged/Andromeda";
+import SpaceFood from "./pages/main/tagged/SpaceFood";
+import SpaceShip from "./pages/main/tagged/SpaceShip";
+import SpaceSuit from "./pages/main/tagged/SpaceSuit";
+import HorizonZero from "./pages/main/tagged/HorizonZero";
 
 function App() {
   const [username, setUsername] = useState("");
   useEffect(() => {
-    console.log(typeof localStorage.getItem("username"));
     localStorage.getItem("username");
   }, []);
+
+  const [selectedTag, setSelectedTag] = useState("");
+
+  const handleTagClick = (tag: string) => {
+    setSelectedTag((value) => (value === tag ? "" : tag));
+  };
+
   return (
     <BrowserRouter>
-      <Header username={username} setUsername={setUsername} />
+      <Header
+        username={username}
+        setUsername={setUsername}
+        selectedTag={selectedTag}
+        handleTagClick={handleTagClick}
+      />
       {/* <Loading /> */}
       <Main>
         <Inner>
           <Routes>
             {/* 메인 */}
-            <Route path="/" element={<MainPage />} />
+            <Route
+              path="/"
+              element={
+                selectedTag === "" ? (
+                  <MainPage />
+                ) : selectedTag === "ALL" ? (
+                  <AllPage />
+                ) : selectedTag === "#태양계 부동산" ? (
+                  <Solar />
+                ) : selectedTag === "#안드로메다 부동산" ? (
+                  <Andromeda />
+                ) : selectedTag === "#우주복" ? (
+                  <SpaceSuit />
+                ) : selectedTag === "#우주식량" ? (
+                  <SpaceFood />
+                ) : selectedTag === "#우주선" ? (
+                  <SpaceShip />
+                ) : (
+                  <HorizonZero />
+                )
+              }
+            />
             <Route path="/product" element={<ProductPage />} />
             <Route path="/product/:id" element={<ProductPage />} />
             <Route path="/cart" element={<CartPage />} />
+            <Route path="/payment" element={<PaymentPage />} />
 
             {/* 관리자 페이지 */}
             <Route path="/admin" element={<AdminPage />} />
             {/* 관리자 디자인 테스트 페이지 */}
             <Route path="/admins" element={<AdminPages />}>
-              <Route path="products" element={<AdminOrderPage />} />
+              <Route path="products" element={<ProductsPage />} />
               <Route path="history" element={<HistoryPage />} />
               <Route path="userlist" element={<UserListPage />} />
             </Route>
