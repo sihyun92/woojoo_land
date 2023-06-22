e9530c440ab045f0af66bfc17a37a062b40b0931
ChaDongmin <cdm1263@gmail.com>
2023-06-22 19:20:40 +0900

    헤더에 프로필 이미지 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,9 +2,9 @@ import { theme } from "../../styles/theme";
 import styled from "styled-components";
 import { IoMdHeart } from "react-icons/io";
 import { MdOutlineShoppingCart, MdSearch } from "react-icons/md";
-import { Link, useLocation } from "react-router-dom";
-import { Dispatch, SetStateAction } from "react";
-import { logout } from "../../lib/API/userAPI";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { logout, check } from "../../lib/API/userAPI";
 import SubHeader from "./SubHeader";
 
 interface IMainPageProps {
@@ -13,12 +13,8 @@ interface IMainPageProps {
   selectedTag: string;
   handleTagClick: any;
 }
-function Header({
-  username,
-  setUsername,
-  selectedTag,
-  handleTagClick,
-}: IMainPageProps) {
+
+function Header({ username, setUsername }: IMainPageProps) {
   const [userImg, setUserImg] = useState("");
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -100,9 +96,18 @@ function Header({
               </Link>
               <Link to="/cart">
                 <MdOutlineShoppingCart />
-              </button>
-              <Link to="/user">프로필</Link>
-            </ButtonWrapper>
+              </Link>
+              <Link to="/user">
+                <UserImg>
+                  {userImg ? (
+                    <img src={userImg} alt="프로필" />
+                  ) : (
+                    <img src="/images/User.png" alt="프로필" />
+                  )}
+                  <img src="/images/User.png" alt="프로필" />
+                </UserImg>
+              </Link>
+            </LinkWrapper>
           </User>
         </HeaderWrapper>
       </HeaderContainer>
