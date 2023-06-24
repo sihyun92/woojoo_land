6e16bf8d052cfaac45df5857fc645a5925bb94cb
Kled <1017yu@daum.net>
2023-06-24 21:20:25 +0900

    검색 결과 컴포넌트 생성

---
+++ src/components/main/MainSearched.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { IProduct, productsList } from "../../lib/API/adminAPI";
+import MainTaggedCard from "./MainTaggedCard";
+
+interface ISearched {
+  inputText: string;
+}
+
+function MainSearched({ inputText }: ISearched) {
+  const title = "검색 결과";
+  const [list, setList] = useState<IProduct[]>([]);
+
+  // 검색값과 item의 title을 비교
+  const searchedTitle = list.filter((item) => item.title?.includes(inputText));
+
+  // 검색값과 item의 tag를 비교
+  const searchedTag = list.filter((item) => item.tags?.includes(inputText));
+
+  // 두 검색 조건을 concat
+  const searchedList = searchedTag.concat(searchedTitle);
+
+  useEffect(() => {
+    async function fetchList() {
+      try {
+        const res = await productsList();
+        setList(res);
+      } catch (error) {
+        console.log("error", error);
+      }
+    }
+    fetchList();
+  }, []);
+  return <MainTaggedCard title={title} list={searchedList} />;
+}
+
+export default MainSearched;
