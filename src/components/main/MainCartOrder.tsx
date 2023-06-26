31e659113f5fff18d4c239d60a005bc5f736c471
Kled <1017yu@daum.net>
2023-06-26 21:38:41 +0900

    장바구니 내 주문서 페이지 금액 계산 로직 추가

--- src/components/main/MainCartOrder.tsx
+++ src/components/main/MainCartOrder.tsx
@@ -1,42 +1,62 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { formatDollar } from "../../lib/Function/commonFn";
 import styled from "styled-components";
 import { TRootState } from "../../modules";
+import Button from "../common/Button";
+import { theme } from "../../styles/theme";
+import { Link } from "react-router-dom";
 
 function MainCartOrder() {
   const cartItem = useSelector((state: TRootState) => state.cartItem);
+  const orderPrice = cartItem.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0,
+  );
+
+  const totalPrice = orderPrice + 3000;
+
   return (
     <Container>
-      <PaymentPrice>
-        <span>결제 금액</span>
-      </PaymentPrice>
-      <hr />
-      <Calculator>
-        <div>
-          <span>주문 금액</span>
-          {formatDollar(cartItem.reduce((acc, item) => acc + item.price, 0))}
-        </div>
-        <div>
-          <span>할인 금액</span>
-        </div>
-        <div>
-          <span>로켓배송비</span>
-          <span>{formatDollar(3000)}</span>
-        </div>
-      </Calculator>
-      <hr />
-      <TotalPrice>
-        <span>총 결제 금액</span>
-        {formatDollar(
-          cartItem.reduce((acc, item) => acc + item.price, 0) + 3000,
-        )}
-      </TotalPrice>
+      <OrderWrapper>
+        <PaymentPrice>
+          <span>결제 금액</span>
+        </PaymentPrice>
+        <hr />
+        <Calculator>
+          <div>
+            <span>주문 금액</span>
+            {formatDollar(orderPrice)}
+          </div>
+          <div>
+            <span>할인 금액</span>
+          </div>
+          <div>
+            <span>로켓배송비</span>
+            <span>{formatDollar(3000)}</span>
+          </div>
+        </Calculator>
+        <hr />
+        <TotalPrice>
+          <span>총 결제 금액</span>
+          {formatDollar(totalPrice)}
+        </TotalPrice>
+      </OrderWrapper>
+      <ButtonWrapper>
+        <Button orange="true">
+          <Link to="/payment">주문하기</Link>
+        </Button>
+      </ButtonWrapper>
     </Container>
   );
 }
 
 const Container = styled.div`
   width: 100%;
+`;
+
+const OrderWrapper = styled.div`
+  padding: 2rem 1rem;
+  border: 1px solid ${theme.colors.gray[3]};
 `;
 
 const PaymentPrice = styled.div`
@@ -67,6 +87,16 @@ const TotalPrice = styled.div`
   font-size: 20px;
   display: flex;
   justify-content: space-between;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 100%;
+
+  > button {
+    width: 100%;
+    height: 3rem;
+    margin-top: 1rem;
+  }
 `;
 
 export default MainCartOrder;
