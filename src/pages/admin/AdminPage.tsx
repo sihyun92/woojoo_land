3a5b98f301e1d1c8d3596fb0e7c90dc97869c5a0
ChaDongmin <cdm1263@gmail.com>
2023-06-26 16:20:59 +0900

    관리자 페이지 접근 제한 추가

--- src/pages/admin/AdminPage.tsx
+++ src/pages/admin/AdminPage.tsx
@@ -1,13 +1,31 @@
 import AdminTemplate from "../../components/admin/AdminTemplate";
 import AdminNav from "../../components/admin/AdminNav";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { theme } from "../../styles/theme";
 import ProductsPage from "./ProductsPage";
 import styled from "styled-components";
 import AdminHeader from "../../components/admin/AdminHeader";
+import { check2 } from "../../lib/API/userAPI";
+import { useEffect } from "react";
 
 function AdminPage() {
   const location = useLocation();
+  const navigate = useNavigate();
+  const ADMIN_EMAIL = process.env.REACT_APP_EMAIL;
+  const ADMIN_NAME = process.env.REACT_APP_DISPLAY_NAME;
+
+  useEffect(() => {
+    isAdmin();
+  });
+
+  const isAdmin = async () => {
+    const res = await check2();
+    if (!(res.email === ADMIN_EMAIL && res.displayName === ADMIN_NAME)) {
+      navigate("/");
+      alert("관리자가 아닙니다.");
+    }
+  };
+
   return (
     <AdminTemplate>
       <Sidebar>
@@ -25,7 +43,10 @@ const Sidebar = styled.div`
   display: flex;
   width: 17.8125rem;
   background-color: ${theme.colors.orange.main};
-  background: linear-gradient(${theme.colors.orange.main}, ${theme.colors.orange.linear});
+  background: linear-gradient(
+    ${theme.colors.orange.main},
+    ${theme.colors.orange.linear}
+  );
 `;
 
 const AdminMain = styled.div`
