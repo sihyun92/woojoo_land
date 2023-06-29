ccf071ebcccf5121422d95a5feb0c135e99120ff
ChaDongmin <cdm1263@gmail.com>
2023-06-29 17:33:21 +0900

    prev next 버튼 스타일링

--- src/components/common/Carousel.tsx
+++ src/components/common/Carousel.tsx
@@ -1,19 +1,25 @@
 import Slider, { Settings } from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import styled from "styled-components";
+import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
 
 interface ICarouselProps {
   children: React.ReactNode;
   slides: number;
+  color: string;
 }
 
-function Carousel({ children, slides }: ICarouselProps) {
+function Carousel({ children, slides, color }: ICarouselProps) {
   const settings: Settings = {
     dots: true,
     infinite: true,
     speed: 300,
     slidesToShow: slides,
     autoplay: true,
+    arrows: true,
+    nextArrow: <MdArrowForwardIos color={color} />,
+    prevArrow: <MdArrowBackIosNew color={color} />,
     responsive: [
       {
         breakpoint: 768,
@@ -30,7 +36,27 @@ function Carousel({ children, slides }: ICarouselProps) {
     ],
   };
 
-  return <Slider {...settings}>{children}</Slider>;
+  return <StyledSlider {...settings}>{children}</StyledSlider>;
 }
+
+const StyledSlider = styled(Slider)`
+  position: relative;
+
+  .slick-prev {
+    position: absolute;
+    left: -80px;
+  }
+
+  .slick-next {
+    position: absolute;
+    right: -70px;
+  }
+
+  .slick-prev::before,
+  .slick-next::before {
+    opacity: 0;
+    display: none;
+  }
+`;
 
 export default Carousel;
