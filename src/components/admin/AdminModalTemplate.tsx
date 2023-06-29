12c2665486e566a98f91c87a21a6b320d9745763
DICEPT <dicepted@gmail.com>
2023-06-29 12:54:11 +0900

    관리자 모달 z-index 순서 변경

--- src/components/admin/AdminModalTemplate.tsx
+++ src/components/admin/AdminModalTemplate.tsx
@@ -12,7 +12,7 @@ function AdminModalTemplate({children}: PropsWithChildren) {
 const ModalBackground = styled.div`
   top: 0;
   left: 0;
-  z-index: 1;
+  z-index: 10;
   width: 100%;
   height: 100%;
   position: fixed;
