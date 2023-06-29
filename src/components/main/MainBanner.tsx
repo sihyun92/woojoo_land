bcc79eb1379a1a000e5ef90bb8eeacba23c15f5c
Kled <1017yu@daum.net>
2023-06-29 13:53:28 +0900

    배너 추가, 기준선 bottom에 맞춤

--- src/components/main/MainBanner.tsx
+++ src/components/main/MainBanner.tsx
@@ -1,22 +1,44 @@
 import styled from "styled-components";
+import Carousel from "../common/Carousel";
+import { theme } from "../../styles/theme";
 
 function Banner() {
   return (
     <Container>
-      <div>Banner</div>
+      <Carousel slides={1}>
+        <Image src="/images/BannerImage1.png" alt="banner1" />
+        <Image src="/images/BannerImage2.png" alt="banner1" />
+        <Image src="/images/BannerImage3.png" alt="banner1" />
+      </Carousel>
     </Container>
   );
 }
 
 const Container = styled.div`
-  width: 100%;
-  height: 26.25rem;
-  background: black;
+  width: 100vw;
   color: white;
   text-align: center;
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: ${theme.colors.black};
+
+  .slick-slider {
+    width: 100rem;
+  }
+
+  .slick-list {
+  }
+
+  .slick-arrow {
+    width: 5rem;
+    height: 4rem;
+    top: 280px;
+  }
+`;
+
+const Image = styled.img`
+  vertical-align: bottom;
 `;
 
 export default Banner;
