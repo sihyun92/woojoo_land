537a86c761a58e2b4cf91e990ea9c15cfb222283
ChaDongmin <cdm1263@gmail.com>
2023-07-01 21:28:46 +0900

    거래내역을 불러오는 로직 구현

--- src/pages/admin/HistroyPage.tsx
+++ src/pages/admin/HistroyPage.tsx
@@ -1,26 +1,23 @@
 import styled from "styled-components";
 import { theme } from "../../styles/theme";
-// import { useEffect, useState } from "react";
-// import { IProduct, salesHistory } from "../../lib/API/adminAPI";
+import { useEffect, useState } from "react";
+import { IOrdalDetailAll, salesHistory } from "../../lib/API/adminAPI";
 import HistroyItem from "../../components/admin/AdminHistroyItem";
 import Button from "../../components/common/Button";
 import AdminTitle from "../../components/admin/AdminTitle";
 
 function HistoryPage() {
-  // 현재 구매내역이 없어서 리스트 API 불러오기 불가
-  // const [list, setList] = useState<IProduct[]>([]);
+  const [lists, setLists] = useState<IOrdalDetailAll[]>([]);
+  const [isChanged, setIsChanged] = useState(false);
 
-  // useEffect(() => {
-  //   async function fetchList() {
-  //     try {
-  //       const res = await salesHistory();
-  //       setList(res);
-  //     } catch (error) {
-  //       console.error();
-  //     }
-  //   }
-  //   fetchList();
-  // }, []);
+  const fetchList = async () => {
+    const res = await salesHistory();
+    setLists(res);
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, [lists, isChanged]);
 
   return (
     <>
@@ -34,11 +31,36 @@ function HistoryPage() {
             <TotalOrderAmount>총 주문 금액</TotalOrderAmount>
             <TransactionTime>거래 시간</TransactionTime>
           </ItemBox>
-          <CancelBtn admin>새로고침</CancelBtn>
+          <CancelBtn
+            admin
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            새로고침
+          </CancelBtn>
         </CategoryMenuContainer>
         <ItemContainer>
           {/* HistroyItem은 개별 아이템 영역, 현재 구매내역이 없어서 리스트 출력 불가 */}
-          <HistroyItem />
+          {lists ? (
+            lists
+              .sort(
+                (a, b) =>
+                  new Date(b.timePaid).getTime() -
+                  new Date(a.timePaid).getTime(),
+              )
+              .map((list) => {
+                return (
+                  <HistroyItem
+                    list={list}
+                    setIsChanged={setIsChanged}
+                    key={list.detailId}
+                  />
+                );
+              })
+          ) : (
+            <ErrorMessage>거래 내역이 없습니다.</ErrorMessage>
+          )}
         </ItemContainer>
       </HistoryContainer>
     </>
@@ -113,8 +135,14 @@ const CancelBtn = styled(Button)`
 // 카테고리 메뉴 스타일 끝
 
 const ItemContainer = styled.ul`
-flex-direction: column;
-display: flex;
+  flex-direction: column;
+  display: flex;
+`;
+
+const ErrorMessage = styled.span`
+  color: ${theme.colors.orange.main};
+  font-weight: 700;
+  font-size: 18px;
 `;
 
 export default HistoryPage;
